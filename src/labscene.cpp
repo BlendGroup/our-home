75114f0e477a7defbeaaaa0682300300b8772dd2
Deep Lalwani <deepthesmartest@gmail.com>
2023-05-22 02:18:12 +0530

    Fixed Cubemap

--- src/labscene.cpp
+++ src/labscene.cpp
@@ -55,8 +55,6 @@ static glshaderprogram* programColor;
 
 static GLuint skybox_vao,vbo;
 
-static GLuint skyboxdemo;
-
 static audioplayer *playerBkgnd;
 static audioplayer *playerRobotThump;
 static bool crt = true;
@@ -125,8 +123,6 @@ void labscene::init() {
 	modelAstro = new glmodel("resources/models/astronaut/MCAnim.glb", aiProcessPreset_TargetRealtime_Quality, true);
 	
 	bspRobot = new BsplineInterpolator(robotSpline);
-
-	skyboxdemo = createTextureCubemap("resources/textures/skybox.jpg");
 
 	envMapper = new CubeMapRenderTarget(CUBEMAP_SIZE, CUBEMAP_SIZE, false);
 	envMapper->setPosition(vec3(0.0f, 0.0f, 0.0f));
@@ -229,8 +225,7 @@ void labscene::render() {
                 modelLab->draw(programStaticPBR,1);
             }
             glBindFramebuffer(GL_FRAMEBUFFER,0);
-            // sceneLightManager->setEnvmap(envMapper->cubemap_texture);
-            sceneLightManager->setEnvmap(skyboxdemo);
+            sceneLightManager->setEnvmap(envMapper->cubemap_texture);
             sceneLightManager->PrecomputeIndirectLighting();
             crt = false;
         }
@@ -293,8 +288,7 @@ void labscene::render() {
         glUniformMatrix4fv(programSkybox->getUniformLocation("pMat"),1,GL_FALSE,programglobal::perspective);
         glUniformMatrix4fv(programSkybox->getUniformLocation("vMat"),1,GL_FALSE,programglobal::currentCamera->matrix());
         glBindVertexArray(skybox_vao);
-        glBindTextureUnit(0,skyboxdemo);
-        // glBindTextureUnit(0,envMapper->cubemap_texture);
+        glBindTextureUnit(0,envMapper->cubemap_texture);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
 #ifdef DEBUG
