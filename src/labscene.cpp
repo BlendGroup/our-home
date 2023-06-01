255c5ced8a39163959c9041f66171b77ef35f14d
KaivalyaD <kaivalya.v.deshpande@gmail.com>
2023-06-02 02:48:46 +0530

    added hologram and random beeps

--- src/labscene.cpp
+++ src/labscene.cpp
@@ -55,6 +55,7 @@ static GLuint emptyvao;
 static audioplayer *playerBkgnd;
 static audioplayer *playerRobotThump;
 static audioplayer *playerOpeningDoor;
+static audioplayer *playerHologramBeeps;
 
 static godrays* godraysDoor;
 
@@ -146,6 +147,7 @@ void labscene::init() {
 	playerBkgnd = new audioplayer("resources/audio/TheLegendOfKai.wav");
 	playerRobotThump = new audioplayer("resources/audio/MetallicThumps.wav");
 	playerOpeningDoor = new audioplayer("resources/audio/OpeningDoor.wav");
+	playerHologramBeeps = new audioplayer("resources/audio/HologramAndBeeps.wav");
 
 	modelLab = new glmodel("resources/models/spaceship/SpaceLab.fbx", aiProcessPreset_TargetRealtime_Quality, true);
 	modelDoor = new glmodel("resources/models/spaceship/door.fbx", aiProcessPreset_TargetRealtime_Quality, true);
@@ -407,6 +409,11 @@ void labscene::update() {
 		playerOpeningDoor->play();
 	} else {
 		playerOpeningDoor->pause();
+	}
+	if(labevents->getT() > 0.00001f && labevents->getT() < 25.0f) {
+		playerHologramBeeps->play();
+	} else {
+		playerHologramBeeps->pause();
 	}
 
 	if((*labevents)[ROBOT_T] >= 0.00001f && (*labevents)[ROBOT_T] <= 0.99999f) {
